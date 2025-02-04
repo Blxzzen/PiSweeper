124ea85cb6f4b7df6b8525aa7a265e0c28a96e3c
Blxzzen <devan@s0dhi.com>
2025-02-03 20:57:42 -0500

    Removed grid spacing

--- pisweeper.cpp
+++ pisweeper.cpp
@@ -7,28 +7,60 @@
 PiSweeper::PiSweeper(QWidget *parent) : QWidget(parent) {
     gridLayout = new QGridLayout(this);
     gridLayout->setSpacing(0);
-    gridLayout->setContentsMargins(0, 0, 0, 0);
+    gridLayout->setContentsMargins(0,0,0,0);
+    gridLayout->setHorizontalSpacing(0);
+    gridLayout->setVerticalSpacing(0);
+
     setupBoard();
 }
 
 void PiSweeper::setupBoard() {
-    QPixmap pixmap(":/images/box.jpg");  // Load the image
-    pixmap = pixmap.scaled(20, 20, Qt::IgnoreAspectRatio, Qt::SmoothTransformation);  // Scale it to the desired size
+    QPixmap pixmap(":/images/default.jpg");
+    pixmap = pixmap.scaled(20, 20, Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
 
     for (int row = 0; row < rows; ++row) {
         QVector<QPushButton*> rowButtons;
         for (int col = 0; col < cols; ++col) {
             QPushButton *button = new QPushButton;
-            button->setIcon(QIcon(pixmap));  // Set the pixmap as an icon
-            button->setIconSize(pixmap.size());  // Ensure the icon size matches the pixmap
-            button->setFixedSize(20, 20);  // Set the fixed size for buttons
+
+            // Prevent resizing issues
+            button->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+            button->setMinimumSize(20, 20);
+            button->setMaximumSize(20, 20);
+            button->setFixedSize(20, 20);
+            
+            // Remove ALL internal spacing or borders
+            button->setStyleSheet("QPushButton {"
+                                  " padding: 0px; "
+                                  " margin: 0px; "
+                                  " border: none; "
+                                  " outline: none; "
+                                  " background: none; "
+                                  " }");
+
+            button->setIcon(QIcon(pixmap));
+            button->setIconSize(pixmap.size());
+            
             gridLayout->addWidget(button, row, col);
             connect(button, &QPushButton::clicked, this, &PiSweeper::buttonClicked);
             rowButtons.push_back(button);
         }
         buttons.push_back(rowButtons);
     }
+
+    // Force the grid to be FIXED and not distribute extra space
+    gridLayout->setSpacing(0);
+    gridLayout->setContentsMargins(0, 0, 0, 0);
+    gridLayout->setSizeConstraint(QLayout::SetFixedSize);
+
+    for (int i = 0; i < rows; ++i)
+        gridLayout->setRowStretch(i, 0);
+    
+    for (int i = 0; i < cols; ++i)
+        gridLayout->setColumnStretch(i, 0);
 }
+
+
 
 void PiSweeper::buttonClicked() {
     QPushButton *clickedButton = qobject_cast<QPushButton*>(sender());
